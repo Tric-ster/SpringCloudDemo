7e1d5890b8ddac8aed62784a36ad449865277fbe
TrickstTT <1509397017@qq.com>
2018-04-25 00:36:47 +0800

    init commit

---
+++ spring-cloud-eureka/src/main/java/com/trickster/springcloudeurka/SpringCloudEurekaApplication.java
@@ -0,0 +1,11 @@
+package com.trickster.springcloudeurka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaApplication.class, args);
+	}
+}
