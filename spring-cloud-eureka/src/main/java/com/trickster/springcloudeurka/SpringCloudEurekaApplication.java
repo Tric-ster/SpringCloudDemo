e118b71031380a27028abf06ffe8f054a79db19f
TrickstTT <1509397017@qq.com>
2018-04-25 23:23:01 +0800

    init commit

--- spring-cloud-eureka/src/main/java/com/trickster/springcloudeurka/SpringCloudEurekaApplication.java
+++ spring-cloud-eureka/src/main/java/com/trickster/springcloudeurka/SpringCloudEurekaApplication.java
@@ -2,8 +2,10 @@ package com.trickster.springcloudeurka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringCloudEurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudEurekaApplication.class, args);
